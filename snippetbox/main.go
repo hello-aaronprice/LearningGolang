041cc3935b69357c1c6caf484bf07e8f3d1daab4
Aaron Price <hello-aaronprice@outlook.com>
2024-11-11 18:56:18 +0800

    update: First basic routes

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -10,10 +10,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Across the pond"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Diplay a form for creating a new snippet..."))
+}
+
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then register the home function as a handler for the "/" URL pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
