90db4af8035d97722ef044264763438f3652151a
Aaron Price <hello-aaronprice@outlook.com>
2024-11-28 18:06:47 +0800

    update: HTML Templating

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"html/template"
+  "log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
-    w.Write([]byte("Hello from Snippetbox"))
+    files := []string{
+    "./ui/html/base.tmpl",
+    "./ui/html/pages/home.tmpl",
+    "./ui/html/partials/nav.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+      log.Print(err.Error())
+      http.Error(w, "Internal Server error", http.StatusInternalServerError)
+      return
+    }
+    err = ts.ExecuteTemplate(w, "base", nil)
+    if err != nil {
+      log.Print(err.Error())
+      http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+  }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
